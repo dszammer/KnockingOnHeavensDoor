9ef5c26198c9c9c9d7248992ffdc933aaf7c6740
stefanreichenauer <stefan-reichenauer@gmx.at>
2016-01-29 21:35:00 +0100

    TestCommit

---
+++ KoHD-UnityProject/Assets/Monster.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+
+    [SerializeField]
+    private int health = 1000;
+    [SerializeField]
+    private int speed = 5;
+
+
+
+    
+    // Use this for initialization
+    void Start () {
+        //this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
