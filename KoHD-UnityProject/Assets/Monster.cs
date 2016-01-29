8b2c6ed6eb8570c8206e1540d1752136e9f5755e
stefanreichenauer <stefan-reichenauer@gmx.at>
2016-01-29 22:37:32 +0100

    WaypointFinding

--- KoHD-UnityProject/Assets/Monster.cs
+++ KoHD-UnityProject/Assets/Monster.cs
@@ -7,15 +7,21 @@ public class Monster : MonoBehaviour {
     [SerializeField]
     private int health = 1000;
     [SerializeField]
-    private int speed = 5;
+    private float speed = 0.5f;
 
-    private Vector3 nextWaypoint;
+    private Transform nextWaypoint;
+    private GameObject WaypointArray;
 
-    
     // Use this for initialization
     void Start () {
         //this.transform.position;
-	}
+        WaypointArray = GameObject.Find("Waypoints");
+        nextWaypoint = WaypointArray.transform.GetChild(4);
+        //Debug.Log(nextWaypoint.GetSiblingIndex());
+        //SetNextWaypoint();
+
+        //Debug.Log(nextWaypoint.GetSiblingIndex());
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -24,12 +30,37 @@ public class Monster : MonoBehaviour {
 
     private void MoveToNextWaypoint()
     {
-        
+        //Debug.Log(gameObject.transform.position);
+        Vector3 actualPos = gameObject.transform.position;
+        Vector3 destinationPos = nextWaypoint.position;
+
+         actualPos += (destinationPos - actualPos).normalized * speed * Time.deltaTime;
+
+        //Vector3 newPos = new Vector3(actualPos.x + (destinationPos.x - actualPos.x) * speed, actualPos.y + (destinationPos.y - actualPos.y) * speed, actualPos.z);
+        gameObject.transform.position = actualPos;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Collided WayPoint" + nextWaypoint.GetSiblingIndex());
+        if (other.gameObject.transform.Equals(nextWaypoint))
+        {
+            Debug.Log("Switch WayPoint" + nextWaypoint.GetSiblingIndex()+1);
+            SetNextWaypoint();
+        }
+        //WaypointArray.transform.
+    }
 
+    void SetNextWaypoint()
+    {
+        if(nextWaypoint.GetSiblingIndex() >= WaypointArray.transform.childCount - 1)
+        {
+            nextWaypoint = WaypointArray.transform.GetChild(0);
+        }
+        else
+        {
+            nextWaypoint = WaypointArray.transform.GetChild(nextWaypoint.GetSiblingIndex() + 1);
+        }
     }
 
 }
