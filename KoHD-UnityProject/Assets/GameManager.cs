436ab600d59a14954258039d95c8b24c705cf0b8
aklinger <klianc09@htlkaindorf.at>
2016-01-30 16:54:38 +0100

    Tried to display remaining time

--- KoHD-UnityProject/Assets/GameManager.cs
+++ KoHD-UnityProject/Assets/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+	public GameObject textField;
+	private GUIText text;
+
 	public int monsterToWin = 100;
 	private int monstersInGoal = 0;
 
@@ -19,12 +22,13 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		text = textField.GetComponents<GUIText>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		levelTimePassed += Time.deltaTime;
+		text.text = "Time Left: " + (levelTimeLimitSeconds - levelTimePassed) + "s";
 		if (levelTimePassed >= levelTimeLimitSeconds) {
 			SceneManager.LoadScene ("loseScreen");
 		}
---
+++ KoHD-UnityProject/Assets/prefabs/ButtonSequences.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ButtonSequences : MonoBehaviour {
+
+	private IList<IList<string>> sequenceCodes;
+
+	// Use this for initialization
+	void Start () {
+		LinkedList sequence = new LinkedList ();
+		sequence.Add ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
